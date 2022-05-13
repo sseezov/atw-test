56be7d64c6eed11087902e181f22b0961adc9bf3
sseezov <sseezov@mail.ru>
2022-05-13 17:19:56 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header">
+          <button>произвести робота</button>
+        </div>
+        <div className="fabric">фабрика по производству биороботов</div>
+        <div className="purse">purse</div>
+        <div className="market">market</div>
+        <div className="storage">storage</div>
+        <div className="production">production</div>
+      </div>
     </div>
   );
 }
