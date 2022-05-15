c4315c17c6f5b2d088a1f5693fb67595164f1d9d
sseezov <sseezov@mail.ru>
2022-05-15 10:09:58 +0700

    fabric fix + purse undone

--- src/components/Purse.tsx
+++ src/components/Purse.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/Purse.scss";
 
 const Purse = () => {
-  return <div>Purse</div>;
+  return (
+    <div className="purse">
+      <div className="purseBorder">
+        <p className="second">02</p>
+      </div>
+      <div className="purseLeft">
+        <h2 className="purseHeader">Кошелек криптовалют</h2>
+        <div className="purseMoney">здесь монетки</div>
+        <p className="purseMoneySubheader">45 biorobo монет</p>
+        <div className="purseGetMoneyBlock">
+          <p className="purseGetMoneyButton">Нацыганить</p>
+          <input type="checkbox" className="purseGetMoneyCheckbox" />
+          <label className="purseGetMoneyLabel">Цыганить по 5 монет</label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Purse;
